2ba987415d231910edf0d49b28e458c34e561f43
komajun <18fi054@ms.dendai.ac.jp>
2023-06-27 12:43:05 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -12,23 +12,24 @@ def main():
         line_thickness=1,
     )
 
-    cap = cv.VideoCapture(0)
+    cap1 = cv.VideoCapture("source-saliency-aware_sample_1m_1.mkv")
+    cap2 = cv.VideoCapture("sample_1m_1.mkv")
+    frame_count = int(cap1.get(cv.CAP_PROP_FRAME_COUNT))
 
-    while True:
-        ret, frame = cap.read()
+    for i in range(frame_count):
+        ret, frame1 = cap1.read()
+        ret, frame2 = cap2.read()
         if not ret:
             continue
-        frame = cv.resize(frame, (960, 540))
-        original_frame = copy.deepcopy(frame)
 
-        frame = cv.Canny(frame, 100, 100)
-        frame = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+        cvwindow.imshow(frame1, frame2)
 
-        cvwindow.imshow(original_frame, frame)
-        key = cv.waitKey(1)
+        key = cv.waitKey(33)
         if key == 27:  # ESC
             break
-    cap.release()
+
+    cap1.release()
+    cap2.release()
     cv.destroyAllWindows()
 
 
